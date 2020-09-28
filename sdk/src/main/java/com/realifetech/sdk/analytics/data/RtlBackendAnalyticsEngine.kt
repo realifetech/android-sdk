47572909d405f0561978aacd9aa42108a49fdc1c
Alex <cebotari.alex@gmail.com>
2020-09-28 21:50:18 +0100

    Add analytics module.

---
+++ sdk/src/main/java/com/realifetech/sdk/analytics/data/RtlBackendAnalyticsEngine.kt
@@ -0,0 +1,10 @@
+package com.realifetech.sdk.analytics.data
+
+import com.realifetech.sdk.analytics.domain.AnalyticsEvent
+import com.realifetech.sdk.domain.Result
+
+class RtlBackendAnalyticsEngine : AnalyticsEngine {
+    override fun logEvent(event: AnalyticsEvent): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+}
