7845c94f956d2a9c2a386f04391917f7542d2705
Alex <cebotari.alex@gmail.com>
2020-10-07 15:39:45 +0100

    Add communicate module

--- sdk/src/main/java/com/realifetech/sdk/Realifetech.kt
+++ sdk/src/main/java/com/realifetech/sdk/Realifetech.kt
@@ -1,9 +1,14 @@
 package com.realifetech.sdk
 
+import com.realifetech.sdk.communicate.Communicate
 import com.realifetech.sdk.general.General
 
 object Realifetech {
     fun getGeneral(): General {
         return General.instance
+    }
+
+    fun getCommunicate(): Communicate {
+        return Communicate.instance
     }
 }
