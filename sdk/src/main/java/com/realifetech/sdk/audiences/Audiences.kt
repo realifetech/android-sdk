a3c9220dc44cade0a3c80ef0d903f8405bac38fa
Alex <cebotari.alex@gmail.com>
2020-11-04 14:16:30 +0000

    Fix crash when HTTP exception is thrown in Audience

--- sdk/src/main/java/com/realifetech/sdk/audiences/Audiences.kt
+++ sdk/src/main/java/com/realifetech/sdk/audiences/Audiences.kt
@@ -1,6 +1,7 @@
 package com.realifetech.sdk.audiences
 
 import com.apollographql.apollo.coroutines.await
+import com.apollographql.apollo.exception.ApolloHttpException
 import com.realifetech.BelongsToAudienceByExternalIdQuery
 import com.realifetech.sdk.core.network.graphQl.GraphQlModule
 import kotlinx.coroutines.Dispatchers
@@ -10,13 +11,21 @@ import kotlinx.coroutines.withContext
 
 class Audiences private constructor() {
 
-    fun deviceIsMemberOfAudience(audienceId: String, callback: (error: Error?, result: Boolean) -> Unit) {
+    fun deviceIsMemberOfAudience(externalAudienceId: String, callback: (error: Error?, result: Boolean) -> Unit) {
         GlobalScope.launch(Dispatchers.IO) {
             val apolloClient = GraphQlModule.apolloClient
-            val responseServer = apolloClient.query(BelongsToAudienceByExternalIdQuery(audienceId)).await()
-            val belongsToAudienceResponse = responseServer.data?.me?.device?.belongsToAudienceByExternalId ?: false
-            val firstServerError = responseServer.errors?.firstOrNull()
-            val errorToReturn = if (firstServerError != null) Error(firstServerError.message) else null
+            var errorToReturn: Error?
+            var belongsToAudienceResponse = false
+
+            try {
+                val responseServer = apolloClient.query(BelongsToAudienceByExternalIdQuery(externalAudienceId)).await()
+                belongsToAudienceResponse = responseServer.data?.me?.device?.belongsToAudienceByExternalId ?: false
+                val firstServerError = responseServer.errors?.firstOrNull()
+                errorToReturn = if (firstServerError != null) Error(firstServerError.message) else null
+            } catch (exception: ApolloHttpException) {
+                val errorMessage =  "HTTP error with code = ${exception.code()} & message = ${exception.message()}"
+                errorToReturn = Error(errorMessage)
+            }
 
             withContext(Dispatchers.Main) {
                 callback(errorToReturn, belongsToAudienceResponse)
