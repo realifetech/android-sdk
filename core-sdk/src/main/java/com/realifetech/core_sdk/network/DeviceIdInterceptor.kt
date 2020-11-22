d6190cc47d543eee0a93bb178ca7a9f6e4728a16
Alex <cebotari.alex@gmail.com>
2020-11-22 22:17:41 +0000

    Add core logic into core-sdk

--- core-sdk/src/main/java/com/realifetech/core_sdk/network/DeviceIdInterceptor.kt
+++ core-sdk/src/main/java/com/realifetech/core_sdk/network/DeviceIdInterceptor.kt
@@ -1,19 +1,17 @@
-package com.realifetech.sdk.core.network
+package com.realifetech.core_sdk.network
 
-import com.realifetech.sdk.RealifeTech
-import com.realifetech.sdk.core.domain.AuthenticationToken
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.io.IOException
 
-internal class DeviceIdInterceptor : Interceptor {
+class DeviceIdInterceptor   (private val deviceId: String) : Interceptor {
 
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val newBuilder = originalRequest.newBuilder()
 
-        newBuilder.addHeader("X-LS-DeviceId", RealifeTech.getGeneral().deviceIdentifier)
+        newBuilder.addHeader("X-LS-DeviceId", deviceId)
 
         return chain.proceed(newBuilder.build())
     }
