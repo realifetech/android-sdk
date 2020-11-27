e094aa98e1ea726a274a16b0e420ad51317a2743
Alex <cebotari.alex@gmail.com>
2020-11-27 15:36:14 +0000

    Add flowable support for getting widgets by screen id

--- core-sdk/src/main/java/com/realifetech/core_sdk/feature/widgets/WidgetsRepository.kt
+++ core-sdk/src/main/java/com/realifetech/core_sdk/feature/widgets/WidgetsRepository.kt
@@ -3,8 +3,15 @@ package com.realifetech.core_sdk.feature.widgets
 import com.realifetech.core_sdk.domain.Result
 import com.realifetech.core_sdk.feature.widgets.domain.Widget
 import com.realifetech.type.ScreenType
+import io.reactivex.Flowable
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.rx2.rxFlowable
 
 class WidgetsRepository(private val dataSource: DataSource) {
+
+    fun getWidgetsByScreenIdFlowable(id: String): Flowable<Result<List<Widget>>> {
+        return rxFlowable { dataSource.getWidgetsByScreenId(id) }
+    }
 
     suspend fun getWidgetsByScreenType(screenType: ScreenType) = dataSource.getWidgetsByScreenType(screenType)
 
