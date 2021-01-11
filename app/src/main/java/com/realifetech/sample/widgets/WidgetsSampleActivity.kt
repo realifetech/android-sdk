e1e543505aee62df86217d131322145dcaa45494
Lokmane Krizou <lokmane.krizou@livestyled.com>
2021-01-11 10:14:53 +0000

    feature/AD-728_Create_widget_and_title_fetching_service

--- app/src/main/java/com/realifetech/sample/widgets/WidgetsSampleActivity.kt
+++ app/src/main/java/com/realifetech/sample/widgets/WidgetsSampleActivity.kt
@@ -3,23 +3,27 @@ package com.realifetech.sample.widgets
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.realifetech.core_sdk.domain.Result
 import com.realifetech.core_sdk.feature.widgets.di.WidgetsModuleProvider
 import com.realifetech.sample.R
 import com.realifetech.sample.data.DeviceConfigurationStorage
+import com.realifetech.sample.utils.disposedBy
+import com.realifetech.sdk.RealifeTech
 import com.realifetech.type.ScreenType
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_widgets_sample.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 class WidgetsSampleActivity : AppCompatActivity() {
 
     private var selectedType: ScreenType? = null
-
+    private val compositeDisposable = CompositeDisposable()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_widgets_sample)
@@ -44,25 +48,73 @@ class WidgetsSampleActivity : AppCompatActivity() {
             }
 
             val storage = DeviceConfigurationStorage(this)
-            GlobalScope.launch(Dispatchers.IO) {
-                val widgetsRepo = WidgetsModuleProvider.provideWidgetsRepository(storage.graphQl)
-                val widgetsResult = widgetsRepo.getWidgetsByScreenType(selectedType!!)
-                when (widgetsResult) {
-                    is Result.Success -> {
 
-                    }
-                    is Result.Error -> {
-                        withContext(Dispatchers.Main) {
+            GlobalScope.launch(Dispatchers.Main) {
+
+                val widgetsRepo = WidgetsModuleProvider.provideWidgetsRepository(
+                    storage.graphQl,
+                    withContext(Dispatchers.IO) { RealifeTech.getGeneral().deviceIdentifier }
+                )
+                widgetsRepo.getWidgetsByScreenTypeFlowable(selectedType!!)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread()).flatMapIterable {
+                        when (it) {
+                            is Result.Success -> {
+                                it.data
+                            }
+                            is Result.Error -> {
+                                throw  it.exception
+                            }
+                        }
+                    }.subscribeBy(
+                        {
                             Toast.makeText(
                                 this@WidgetsSampleActivity,
-                                "Error in loading: ${widgetsResult.exception.message}",
+                                "Error loading: ${it.message}",
                                 Toast.LENGTH_LONG
                             ).show()
-                        }
-                    }
-                }
+                        }, {
+                            Log.d("Completion", "Completed")
+
+                        }, {
+                            Toast.makeText(
+                                this@WidgetsSampleActivity,
+                                "Widget loading: $it",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }).disposedBy(compositeDisposable)
             }
+
+// In case we have coroutines instead of RxJava, you can do the following
+
+//            GlobalScope.launch(Dispatchers.IO) {
+//                CoreConfiguration.deviceId = General.instance.deviceIdentifier
+//                val widgetsRepo = WidgetsModuleProvider.provideWidgetsRepository(storage.graphQl)
+//                val widgetsResult = widgetsRepo.getWidgetsByScreenType(selectedType!!)
+//                when (widgetsResult) {
+//                    is Result.Success -> {
+//                        widgetsResult.data.forEach {
+//                            Log.d("Result", it.toString())
+//                        }
+//
+//                    }
+//                    is Result.Error -> {
+//                        withContext(Dispatchers.Main) {
+//                            Toast.makeText(
+//                                this@WidgetsSampleActivity,
+//                                "Error in loading: ${widgetsResult.exception.message}",
+//                                Toast.LENGTH_LONG
+//                            ).show()
+//                        }
+//                    }
+//                }
+//            }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
     }
 
     companion object {
--- core-sdk/src/main/java/com/realifetech/core_sdk/feature/widgets/di/WidgetsModuleProvider.kt
+++ core-sdk/src/main/java/com/realifetech/core_sdk/feature/widgets/di/WidgetsModuleProvider.kt
@@ -1,11 +1,13 @@
 package com.realifetech.core_sdk.feature.widgets.di
 
+import com.realifetech.core_sdk.domain.CoreConfiguration
 import com.realifetech.core_sdk.feature.widgets.WidgetsRepository
 import com.realifetech.core_sdk.feature.widgets.data.WidgetsBackendDataSource
 import com.realifetech.core_sdk.network.graphQl.GraphQlModule
 
 object WidgetsModuleProvider {
-    fun provideWidgetsRepository(baseUrl: String): WidgetsRepository {
+    fun provideWidgetsRepository(baseUrl: String, deviceId: String): WidgetsRepository {
+        CoreConfiguration.deviceId = deviceId
         val client = GraphQlModule.getApolloClient(baseUrl)
         return WidgetsRepository(WidgetsBackendDataSource(client))
     }
