d06a8bafacb55bf24e13dc3c0a5925e81f851bda
Lokmane Krizou <lokmane.krizou@livestyled.com>
2021-01-11 11:55:55 +0000

    added fetching mechanism for screen title

--- core-sdk/src/main/java/com/realifetech/core_sdk/feature/widgets/data/WidgetsBackendDataSource.kt
+++ core-sdk/src/main/java/com/realifetech/core_sdk/feature/widgets/data/WidgetsBackendDataSource.kt
@@ -4,20 +4,27 @@ import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.api.Error
 import com.apollographql.apollo.coroutines.await
 import com.apollographql.apollo.exception.ApolloHttpException
+import com.realifetech.GetScreenByIdQuery
+import com.realifetech.GetScreenByScreenTypeQuery
 import com.realifetech.GetWidgetsByScreenIdQuery
 import com.realifetech.GetWidgetsByScreenTypeQuery
 import com.realifetech.core_sdk.domain.Result
 import com.realifetech.core_sdk.feature.widgets.WidgetsRepository
+import com.realifetech.core_sdk.feature.widgets.domain.Translation
 import com.realifetech.core_sdk.feature.widgets.domain.Widget
+import com.realifetech.core_sdk.feature.widgets.domain.mapToTranslation
 import com.realifetech.fragment.FragmentWidget
 import com.realifetech.type.ScreenType
 
-class WidgetsBackendDataSource(private val apolloClient: ApolloClient) : WidgetsRepository.DataSource {
+class WidgetsBackendDataSource(private val apolloClient: ApolloClient) :
+    WidgetsRepository.DataSource {
 
     override suspend fun getWidgetsByScreenType(screenType: ScreenType): Result<List<Widget>> {
         return try {
             val response = apolloClient.query(GetWidgetsByScreenTypeQuery(screenType)).await()
-            response.data?.getWidgetsByScreenType?.fragments?.fragmentWidget.extractResponse(response.errors)
+            response.data?.getWidgetsByScreenType?.fragments?.fragmentWidget.extractResponse(
+                response.errors
+            )
         } catch (exception: ApolloHttpException) {
             Result.Error(exception)
         }
@@ -27,6 +34,34 @@ class WidgetsBackendDataSource(private val apolloClient: ApolloClient) : Widgets
         return try {
             val response = apolloClient.query(GetWidgetsByScreenIdQuery(id)).await()
             response.data?.getWidgetsByScreenId?.fragments?.fragmentWidget.extractResponse(response.errors)
+        } catch (exception: ApolloHttpException) {
+            Result.Error(exception)
+        }
+    }
+
+    override suspend fun getScreenByScreenType(screenType: ScreenType): Result<List<Translation>> {
+        return try {
+            val response = apolloClient.query(GetScreenByScreenTypeQuery(screenType)).await()
+            response.data?.getScreenByScreenType?.let {
+                Result.Success(it.translations.map { result -> result.mapToTranslation() })
+            } ?: run {
+                val errorMessage = response.errors?.firstOrNull()?.message ?: "Unknown error"
+                Result.Error(RuntimeException(errorMessage))
+            }
+        } catch (exception: ApolloHttpException) {
+            Result.Error(exception)
+        }
+    }
+
+    override suspend fun getScreenById(id: String): Result<List<Translation>> {
+        return try {
+            val response = apolloClient.query(GetScreenByIdQuery(id)).await()
+            response.data?.getScreenById?.let {
+                Result.Success(it.translations.map { result -> result.mapToTranslation() })
+            } ?: run {
+                val errorMessage = response.errors?.firstOrNull()?.message ?: "Unknown error"
+                Result.Error(RuntimeException(errorMessage))
+            }
         } catch (exception: ApolloHttpException) {
             Result.Error(exception)
         }
