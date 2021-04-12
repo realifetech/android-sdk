a1f602e2f0c8d3a1a990a91ac71fe5309b3f6745
julias <julia.sotola@livestyled.com>
2021-04-12 15:54:09 +0100

    Added nullability to wrapper objects

--- core-sdk/src/main/java/com/realifetech/core_sdk/feature/payment/data/PaymentSourceWrapper.kt
+++ core-sdk/src/main/java/com/realifetech/core_sdk/feature/payment/data/PaymentSourceWrapper.kt
@@ -5,13 +5,13 @@ import com.realifetech.type.CardInput
 import com.realifetech.type.PaymentSourceInput
 
 data class PaymentSourceWrapper(
-    val id: String,
-    val billingDetailsInput: PaymentSourceBillingDetailsWrapper,
-    val card: CardInput
+    val id: String? = null,
+    val billingDetailsInput: PaymentSourceBillingDetailsWrapper? = null,
+    val card: CardInput? = null
 )
 
 fun PaymentSourceWrapper.toInputObject() = PaymentSourceInput(
     Input.optional(id),
-    Input.optional(billingDetailsInput.toInputObject()),
+    Input.optional(billingDetailsInput?.toInputObject()),
     Input.optional(card)
 )
