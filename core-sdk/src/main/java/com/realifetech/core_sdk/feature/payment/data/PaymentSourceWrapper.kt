22097e3eb09ae6014660e5b7406f84b4fa6ab564
julias <julia.sotola@livestyled.com>
2021-04-12 16:17:30 +0100

    Added wrapper for CardInput

--- core-sdk/src/main/java/com/realifetech/core_sdk/feature/payment/data/PaymentSourceWrapper.kt
+++ core-sdk/src/main/java/com/realifetech/core_sdk/feature/payment/data/PaymentSourceWrapper.kt
@@ -1,17 +1,16 @@
 package com.realifetech.core_sdk.feature.payment.data
 
 import com.apollographql.apollo.api.Input
-import com.realifetech.type.CardInput
 import com.realifetech.type.PaymentSourceInput
 
 data class PaymentSourceWrapper(
     val id: String? = null,
     val billingDetailsInput: PaymentSourceBillingDetailsWrapper? = null,
-    val card: CardInput? = null
+    val card: CardWrapper? = null
 )
 
 fun PaymentSourceWrapper.toInputObject() = PaymentSourceInput(
     Input.optional(id),
     Input.optional(billingDetailsInput?.toInputObject()),
-    Input.optional(card)
+    Input.optional(card?.toInputObject())
 )
