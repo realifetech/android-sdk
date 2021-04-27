5aadbf44117ec3e56c4d7d494b469235ff5a4309
Lokmane Krizou <lokmane.krizou@livestyled.com>
2021-04-27 17:32:53 +0100

    update graphQL schema

--- core-sdk/src/main/java/com/realifetech/core_sdk/feature/payment/data/PaymentSourceBillingDetailsWrapper.kt
+++ core-sdk/src/main/java/com/realifetech/core_sdk/feature/payment/data/PaymentSourceBillingDetailsWrapper.kt
@@ -1,14 +1,17 @@
 package com.realifetech.core_sdk.feature.payment.data
 
+import android.os.Parcelable
 import com.apollographql.apollo.api.toInput
 import com.realifetech.type.PaymentSourceBillingDetailsInput
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class PaymentSourceBillingDetailsWrapper(
     val address: PaymentSourceAddressWrapper? = null,
     val email: String? = null,
     val name: String? = null,
     val phone: String? = null
-)
+) : Parcelable
 
 fun PaymentSourceBillingDetailsWrapper.toInputObject() =
     PaymentSourceBillingDetailsInput(
