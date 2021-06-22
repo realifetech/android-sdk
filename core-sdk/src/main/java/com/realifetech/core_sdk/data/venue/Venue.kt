91fd62aad3b53b3e8a5300d44b5f84bbf66509e5
julias <julia.sotola@livestyled.com>
2021-06-22 10:19:55 +0100

    Removed default values for Venue

--- core-sdk/src/main/java/com/realifetech/core_sdk/data/venue/Venue.kt
+++ core-sdk/src/main/java/com/realifetech/core_sdk/data/venue/Venue.kt
@@ -6,30 +6,25 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Venue(
-    var id: String = "",
+    var id: String,
     val isDefault: Boolean? = false,
-    var name: String? = "",
-    val label: String? = "",
-    val description: String? = "",
-    val imageUrl: String? = "",
-    val mapImageUrl: String? = "",
-    val geoLat: String? = "",
-    val geoLong: String? = "",
-    val geoLatNorthWest: String? = "",
-    val geoLongNorthWest: String? = "",
-    val geoLatSouthEast: String? = "",
-    val geoLongSouthEast: String? = "",
-    val city: String? = "",
-    val usefulInfo: List<UsefulInfo>? = emptyList(),
-    val socialMedia: List<SocialMediaResponse>? = emptyList(),
-    val status: String? = null,
-    val externalId: String? = null,
-    val createdAt: String? = null,
-    val updatedAt: String? = null,
-    val venueIconUrl: String? = null
-) : WidgetItem(), Parcelable {
-
-    constructor(selectedVenueId: Int) : this() {
-        this.id = selectedVenueId.toString()
-    }
-}
+    var name: String?,
+    val label: String?,
+    val description: String?,
+    val imageUrl: String?,
+    val mapImageUrl: String?,
+    val geoLat: String?,
+    val geoLong: String?,
+    val geoLatNorthWest: String?,
+    val geoLongNorthWest: String?,
+    val geoLatSouthEast: String?,
+    val geoLongSouthEast: String?,
+    val city: String?,
+    val usefulInfo: List<UsefulInfo>?,
+    val socialMedia: List<SocialMediaResponse>?,
+    val status: String?,
+    val externalId: String?,
+    val createdAt: String?,
+    val updatedAt: String?,
+    val venueIconUrl: String?
+) : WidgetItem(), Parcelable
