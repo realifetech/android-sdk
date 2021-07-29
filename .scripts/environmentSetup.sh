1015f90d74bdd95f4dbefdd9e33d46fba6bd9890
lokmane.krizou <lokmane.krizou@realifetech.com>
2021-07-29 09:30:03 +0100

    update logic to push build when create release

--- .scripts/environmentSetup.sh
+++ .scripts/environmentSetup.sh
@@ -11,7 +11,9 @@ elif [[ $1 =~ ^release\/.+$ || $1 =~ /^hotfix\/.+$/ ]]; then
   fi
 echo "export SDK_CHANGED=$(git diff --quiet HEAD "$( git rev-parse --short @~)" -- sdk || echo changed)" >> "$BASH_ENV";
 echo "export CORE_CHANGED=$(git diff --quiet HEAD "$( git rev-parse --short @~)" -- core-sdk || echo changed)" >> "$BASH_ENV";
-echo "export RELEASE_CREATED=$([ -z "$( git cherry -v develop )" ] && echo "created")" >> "$BASH_ENV";
+if [[ $1 =~ ^release\/.+$ ]]; then
+  echo "export RELEASE_CREATED=$([ -z "$( git cherry -v develop )" ] && echo "created")" >> "$BASH_ENV";
+  fi
 source $BASH_ENV
 echo "$CORE_CHANGED"
 echo "$SDK_CHANGED"
