ce0be2ada503ff0c03360af535b25f4d1834c080
lokmane.krizou <lokmane.krizou@realifetech.com>
2021-08-03 15:55:08 +0100

    feature/AD-954_implement_getMyPaymentIntent

--- core-sdk/src/main/java/com/realifetech/core_sdk/feature/payment/data/PaymentDataSource.kt
+++ core-sdk/src/main/java/com/realifetech/core_sdk/feature/payment/data/PaymentDataSource.kt
@@ -1,6 +1,7 @@
 package com.realifetech.core_sdk.feature.payment.data
 
 import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.api.Input
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.exception.ApolloException
@@ -10,15 +11,15 @@ import com.realifetech.*
 import com.realifetech.core_sdk.data.payment.model.PaymentSource
 import com.realifetech.core_sdk.data.payment.model.asModel
 import com.realifetech.core_sdk.data.shared.`object`.PaginatedObject
+import com.realifetech.core_sdk.domain.Result
 import com.realifetech.core_sdk.feature.payment.PaymentRepository
-import com.realifetech.core_sdk.network.graphQl.GraphQlModule
 import com.realifetech.fragment.PaymentIntent
 import com.realifetech.type.PaymentIntentInput
 import com.realifetech.type.PaymentIntentUpdateInput
 import com.realifetech.type.PaymentSourceInput
 
-class PaymentDataSource() : PaymentRepository.DataSource {
-    private val apolloClient = GraphQlModule.apolloClient
+class PaymentDataSource(val apolloClient: ApolloClient) : PaymentRepository.DataSource {
+
 
     override fun addPaymentSource(
         input: PaymentSourceInput,
@@ -123,6 +124,35 @@ class PaymentDataSource() : PaymentRepository.DataSource {
         } catch (exception: ApolloHttpException) {
             callback.invoke(exception, null)
         }
+    }
+
+    override fun getMyPaymentIntent(
+        id: String
+    ): Result<PaymentIntent>? {
+        var result: Result<PaymentIntent>? = null
+        try {
+            val response = apolloClient.query(GetMyPaymentIntentQuery(id))
+                .toBuilder()
+                .responseFetcher(ApolloResponseFetchers.NETWORK_ONLY)
+                .build()
+            response.enqueue(object :
+                ApolloCall.Callback<GetMyPaymentIntentQuery.Data>() {
+                override fun onResponse(response: Response<GetMyPaymentIntentQuery.Data>) {
+                    response.data?.getMyPaymentIntent?.fragments?.paymentIntent?.let {
+                        result = Result.Success(it)
+                    } ?: run {
+                        result = Result.Error(Exception())
+                    }
+                }
+
+                override fun onFailure(e: ApolloException) {
+                    result = Result.Error(e)
+                }
+            })
+        } catch (exception: ApolloHttpException) {
+            result = Result.Error(exception)
+        }
+        return result
     }
 
     override fun deleteMyPaymentSource(
