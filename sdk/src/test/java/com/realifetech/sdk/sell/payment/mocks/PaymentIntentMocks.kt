997154ae502ade9e56dfa05889470b09c0bbffe0
lokmane.krizou <lokmane.krizou@realifetech.com>
2021-08-11 18:12:49 +0100

    add tests to sdk

---
+++ sdk/src/test/java/com/realifetech/sdk/sell/payment/mocks/PaymentIntentMocks.kt
@@ -0,0 +1,44 @@
+package com.realifetech.sdk.sell.payment.mocks
+
+import com.realifetech.core_sdk.data.payment.wrapper.PaymentIntentUpdateWrapper
+import com.realifetech.core_sdk.data.payment.wrapper.PaymentIntentWrapper
+import com.realifetech.fragment.PaymentIntent
+import com.realifetech.type.OrderType
+import com.realifetech.type.PaymentStatus
+import com.realifetech.type.UpdatePaymentStatus
+
+object PaymentIntentMocks {
+
+    val paymentIntent = PaymentIntent(
+        "PaymentIntent",
+        "",
+        OrderType.FOOD_AND_BEVERAGE,
+        "123",
+        PaymentStatus.SUCCEEDED,
+        null,
+        123,
+        "",
+        false,
+        null,
+        false,
+        null,
+        null
+    )
+    val paymentIntentWrapper = PaymentIntentWrapper(
+        OrderType.FOOD_AND_BEVERAGE,
+        "123",
+        null,
+        123,
+        "",
+        false,
+        false,
+        null,
+        null,
+    )
+
+    val paymentIntentUpdateWrapper = PaymentIntentUpdateWrapper(
+        UpdatePaymentStatus.CANCELED,
+        null,
+        null
+    )
+}
