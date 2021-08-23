79692cdeb0323049b6ab8bb50dcd7d21de669a47
lokmane.krizou <lokmane.krizou@realifetech.com>
2021-08-23 12:25:04 +0100

    quick fix for sample app

--- app/src/main/java/com/realifetech/sample/SampleApplication.kt
+++ app/src/main/java/com/realifetech/sample/SampleApplication.kt
@@ -1,11 +1,8 @@
 package com.realifetech.sample
 
 import android.app.Application
-import android.graphics.Color
-import androidx.core.content.ContextCompat
 import com.realifetech.sample.data.DeviceConfigurationStorage
 import com.realifetech.sdk.RealifeTech
-import com.realifetech.sdk.general.data.color.ColorType
 
 class SampleApplication : Application() {
     override fun onCreate() {
@@ -14,9 +11,10 @@ class SampleApplication : Application() {
         val storage = DeviceConfigurationStorage(this)
 
         // Prefill the storage with default values from the configuration
-        if (storage.graphQl.isBlank() && storage.apiUrl.isBlank()) {
+        if (storage.graphQl.isBlank() && storage.apiUrl.isBlank() && storage.orderingJourney.isBlank()) {
             storage.graphQl = RealifeTech.getGeneral().configuration.graphApiUrl
             storage.apiUrl = RealifeTech.getGeneral().configuration.apiUrl
+            storage.orderingJourney = RealifeTech.getGeneral().configuration.webOrderingJourneyUrl
         }
 
         RealifeTech.getGeneral().configuration.apply {
@@ -25,7 +23,7 @@ class SampleApplication : Application() {
             graphApiUrl = storage.graphQl
             clientSecret = storage.clientSecret
             appCode = storage.clientId
-            webOrderingJourneyUrl=storage.orderingJourney
+            webOrderingJourneyUrl = storage.orderingJourney
         }
 //  Set Colors via code
         // EXAMPLE 1
