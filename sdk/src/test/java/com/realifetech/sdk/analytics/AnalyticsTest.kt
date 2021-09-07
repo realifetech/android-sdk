c0e293e98978f337e80eaa6663c25a176836cd97
lokmane.krizou <lokmane.krizou@realifetech.com>
2021-09-07 17:47:16 +0100

    feature/WOD-19_Analytics_test_cover100%

---
+++ sdk/src/test/java/com/realifetech/sdk/analytics/AnalyticsTest.kt
@@ -0,0 +1,15 @@
+package com.realifetech.sdk.analytics
+
+import org.junit.Before
+import org.junit.Test
+
+class AnalyticsTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun logEvent() {
+    }
+}
---
+++ sdk/src/test/java/com/realifetech/sdk/analytics/domain/RltBackendAnalyticsEngineTest.kt
@@ -0,0 +1,17 @@
+package com.realifetech.sdk.analytics.domain
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class RltBackendAnalyticsEngineTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun logEvent() {
+    }
+}
