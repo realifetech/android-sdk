f4c09a9b9d8d63b711847d98ccf8655daf2aca58
lokmane.krizou <lokmane.krizou@realifetech.com>
2021-09-07 15:08:53 +0100

    feature/WOD-15_Order_Repository

--- sdk/src/test/java/com/realifetech/sdk/sell/order/mocks/OrdersMocks.kt
+++ sdk/src/test/java/com/realifetech/sdk/sell/order/mocks/OrdersMocks.kt
@@ -4,9 +4,9 @@ import com.realifetech.GetMyOrdersQuery
 import com.realifetech.fragment.FragmentOrder
 import com.realifetech.sdk.core.data.model.order.model.asModel
 import com.realifetech.sdk.core.data.model.order.wrapper.OrderUpdateWrapper
+import com.realifetech.sdk.core.data.model.shared.`object`.PaginatedObject
 import com.realifetech.type.CollectionPreferenceType
 import com.realifetech.type.OrderStatus
-import io.mockk.mockk
 
 object OrdersMocks {
 
@@ -20,9 +20,12 @@ object OrdersMocks {
     )
     val orderResponseWithEmptyEdge: List<GetMyOrdersQuery.Edge?> =
         listOf(null, generateOrderEdge(order1))
+
     val wrappedOrdersWithEmptyEdge =
         orderResponseWithEmptyEdge.map { it?.fragments?.fragmentOrder?.asModel }
 
+    val paginatedObject =
+        PaginatedObject(ordersResponse.edges?.map { it?.fragments?.fragmentOrder?.asModel }, 3)
 
 
     private fun generateOrderEdge(order: FragmentOrder) =
