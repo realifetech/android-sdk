260729debb1184ed7bc3ca553aea2eed6abf805e
lokmane.krizou <lokmane.krizou@realifetech.com>
2021-09-13 17:01:05 +0100

    test 100% basket feature

---
+++ sdk/src/test/java/com/realifetech/sdk/sell/basket/data/BasketDataSourceImplTest.kt
@@ -0,0 +1,25 @@
+package com.realifetech.sdk.sell.basket.data
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class BasketDataSourceImplTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getBasket() {
+    }
+
+    @Test
+    fun createMyBasket() {
+    }
+
+    @Test
+    fun updateMyBasket() {
+    }
+}
---
+++ sdk/src/test/java/com/realifetech/sdk/sell/basket/domain/BasketRepositoryTest.kt
@@ -0,0 +1,25 @@
+package com.realifetech.sdk.sell.basket.domain
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class BasketRepositoryTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getBasket() {
+    }
+
+    @Test
+    fun createMyBasket() {
+    }
+
+    @Test
+    fun updateMyBasket() {
+    }
+}
