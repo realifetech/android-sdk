6e6a01f47797bf8b0b4c594a276627e77f9fda51
julias <julia.sotola@livestyled.com>
2021-10-07 09:31:11 +0100

    feature/MOD-191-UpdateMyBasket_mutation_updated

--- sdk/src/main/java/com/realifetech/sdk/core/data/model/basket/BasketRequestItem.kt
+++ sdk/src/main/java/com/realifetech/sdk/core/data/model/basket/BasketRequestItem.kt
@@ -1,6 +1,7 @@
 package com.realifetech.sdk.core.data.model.basket
 
 data class BasketRequestItem(
+    val id: Int,
     val product: String?,
     val productVariant: String?,
     val fulfilmentPoint: String?,
