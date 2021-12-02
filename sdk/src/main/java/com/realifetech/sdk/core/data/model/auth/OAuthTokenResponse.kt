c411a156ac9418b5ed4ddfedb91c986c708d8324
lokmane.krizou <lokmane.krizou@realifetech.com>
2021-12-02 15:56:43 +0000

    small fix

--- sdk/src/main/java/com/realifetech/sdk/core/data/model/auth/OAuthTokenResponse.kt
+++ sdk/src/main/java/com/realifetech/sdk/core/data/model/auth/OAuthTokenResponse.kt
@@ -11,7 +11,7 @@ data class OAuthTokenResponse(
     @SerializedName("refresh_token") val refreshToken: String,
     @SerializedName("refresh_expiry") val refreshExpiry: Date?
 ) {
-    val hasRefreshToken = refreshToken.isNotBlank()
+    val hasRefreshToken get() =  refreshToken.isNotBlank()
     val isTokenExpired: Boolean
         get() {
             refreshExpiry?.let {
