b1000c8a4cfaac163c47118987fcadfa271f3e9f
adriannieves <adrian.nieves@audienceview.com>
2023-02-07 16:44:11 -0300

    resolve conflict

--- sdk/src/main/java/com/realifetech/sdk/access/data/AccessDataSourceImpl.kt
+++ sdk/src/main/java/com/realifetech/sdk/access/data/AccessDataSourceImpl.kt
@@ -21,55 +21,6 @@ import javax.inject.Inject
 
 class AccessDataSourceImpl @Inject constructor(apolloClient: ApolloClient) : AccessDataSource {
 
-    override fun getMyTickets(
-        pageSize: Int,
-        callback: (error: Exception?, response: PaginatedObject<Ticket?>?) -> Unit
-    ) {
-        try {
-            val response = apolloClient.query(GetMyTicketsQuery(pageSize))
-                .toBuilder()
-                .responseFetcher(ApolloResponseFetchers.CACHE_AND_NETWORK)
-                .build()
-
-            response.enqueue(object : ApolloCall.Callback<GetMyTicketsQuery.Data>(){
-                override fun onResponse(response: Response<GetMyTicketsQuery.Data>) {
-                    val tickets = response.data?.getMyTickets?.edges?.map { result -> result?.fragments?.fragmentTicket?.asModel }
-                    val nextPage = response.data?.getMyTickets?.nextPage
-                    val paginatedObject = PaginatedObject(tickets, nextPage)
-                    callback.invoke(null, paginatedObject)
-                }
-
-                override fun onFailure(e: ApolloException) {
-                    callback.invoke(e, null)
-                }
-            })
-        } catch (exception: ApolloHttpException) {
-            callback.invoke(exception, null)
-        }
-    }
-
-    override fun getMyTicketById(
-        id: Int,
-        callback: (error: Exception?, response: Ticket?) -> Unit
-    ) {
-        try {
-            val response = apolloClient.query(GetMyTicketQuery(id = id.toString())).toBuilder()
-                .responseFetcher(ApolloResponseFetchers.CACHE_AND_NETWORK).build()
-            response.enqueue(object : ApolloCall.Callback<GetMyTicketQuery.Data>() {
-                override fun onResponse(response: Response<GetMyTicketQuery.Data>) {
-                    val ticket = response.data?.getMyTicket?.fragments?.fragmentTicket?.asModel
-                    callback.invoke(null, ticket)
-                }
-
-                override fun onFailure(e: ApolloException) {
-                    callback.invoke(e, null)
-                }
-
-            })
-        } catch(exception: ApolloHttpException) {
-            callback.invoke(exception, null)
-        }
-    }
 
     override fun getNextUpcomingTicket(callback: (error: Exception?, ticket: Ticket?) -> Unit) {
         try {
