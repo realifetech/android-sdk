89cc746f85fc8bd11bd3f6b59bf3af9f39bad5e2
adriannieves <adrian.nieves@audienceview.com>
2023-03-19 09:08:07 -0300

    new version

--- sdk/src/main/java/com/realifetech/sdk/core/network/DeviceIdInterceptor.kt
+++ sdk/src/main/java/com/realifetech/sdk/core/network/DeviceIdInterceptor.kt
@@ -14,7 +14,8 @@ class DeviceIdInterceptor @Inject constructor(
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val newBuilder = originalRequest.newBuilder()
-        newBuilder.addHeader(DEVICE_ID_HEADER, storage.deviceId)
+        /*newBuilder.addHeader(DEVICE_ID_HEADER, storage.deviceId)*/
+        newBuilder.addHeader(DEVICE_ID_HEADER, "f341256a-f5e4-4a38-9f0e-401be93a00cf:com.concertlive.core.debug")
         return chain.proceed(newBuilder.build())
     }
 
