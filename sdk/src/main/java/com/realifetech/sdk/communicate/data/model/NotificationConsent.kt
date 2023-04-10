963c3e505a9b8594d2edc28954867ad13a4c77ac
adriannieves <adrian.nieves@audienceview.com>
2023-04-10 12:09:16 -0400

    Add notification consent query and mutation

---
+++ sdk/src/main/java/com/realifetech/sdk/communicate/data/model/NotificationConsent.kt
@@ -0,0 +1,9 @@
+package com.realifetech.sdk.communicate.data.model
+
+data class NotificationConsent(
+    val id: String,
+    val name: String,
+    val sortId: Int,
+    //val translations: [ConsentTranslation],
+    //val status: ConsentStatus
+)
