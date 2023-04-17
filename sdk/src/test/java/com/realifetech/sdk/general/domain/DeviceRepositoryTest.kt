72bc56ad08b5e64f9d877e49599eae8a271bb486
adriannieves <adrian.nieves@audienceview.com>
2023-04-17 08:57:38 -0400

    Unit test

--- sdk/src/test/java/com/realifetech/sdk/general/domain/DeviceRepositoryTest.kt
+++ sdk/src/test/java/com/realifetech/sdk/general/domain/DeviceRepositoryTest.kt
@@ -1,9 +1,12 @@
 package com.realifetech.sdk.general.domain
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.realifetech.sdk.core.data.model.exceptions.NetworkException
 import com.realifetech.sdk.core.domain.OAuthManager
 import com.realifetech.sdk.core.utils.Result
+import com.realifetech.sdk.general.data.DeviceConsent
 import com.realifetech.sdk.general.data.DeviceNetworkDataSource
+import com.realifetech.sdk.general.data.LocationGranularStatus
 import com.realifetech.sdk.general.mocks.GeneralMocks.deviceRegisterResponse
 import com.realifetech.sdk.general.mocks.GeneralMocks.networkError
 import io.mockk.MockKAnnotations
@@ -12,7 +15,10 @@ import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.verify
 import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
 
 class DeviceRepositoryTest {
 
@@ -22,29 +28,42 @@ class DeviceRepositoryTest {
     @RelaxedMockK
     lateinit var oAuthManager: OAuthManager
 
-    private lateinit var deviceRepository: DeviceRepository
+    // Rule para forzar la ejecución de tareas en el hilo principal
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    // Objeto a probar
+    lateinit var deviceRepository: DeviceRepository
+
+    // Datos de prueba
+    lateinit var deviceConsent: DeviceConsent
 
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
         deviceRepository = DeviceRepository(deviceNetworkDataSource) { oAuthManager }
+        // Inicialización de Mockito
+        MockitoAnnotations.initMocks(this)
+
+        // Creación de los datos de prueba
+        deviceConsent = DeviceConsent(true, true, true, LocationGranularStatus.ALWAYS, true, true)
     }
 
     @Test
     fun `register Device results successfully`() {
-        every { deviceNetworkDataSource.registerDevice() } returns Result.Success(
+        every { deviceRepository.registerDevice() } returns Result.Success(
             deviceRegisterResponse
         )
         every { oAuthManager.ensureActive() }returns Unit
         val result = deviceRepository.registerDevice()
         assert(result is Result.Success)
         assertEquals(deviceRegisterResponse, (result as Result.Success).data)
-        verify { deviceNetworkDataSource.registerDevice() }
+        verify { deviceRepository.registerDevice() }
     }
 
     @Test
     fun `register Device result with error `() {
-        every { deviceNetworkDataSource.registerDevice() }
+        every { deviceRepository.registerDevice() }
             .returns(Result.Error(networkError))
         val result = deviceRepository.registerDevice()
         assert(result is Result.Error)
@@ -52,6 +71,36 @@ class DeviceRepositoryTest {
         assertEquals(networkError.message, result.exception.message)
         verify {
             oAuthManager.ensureActive()
-            deviceNetworkDataSource.registerDevice() }
+            deviceRepository.registerDevice() }
     }
+
+    @Test
+    fun `test update my device consent Success`() {
+        `when`(deviceNetworkDataSource.updateMyDeviceConsent(deviceConsent,
+            Any() as (Exception?, Boolean?) -> Unit
+        )).thenAnswer {
+            val callback = it.arguments[1] as (Throwable?, Boolean?) -> Unit
+            callback(null, true)
+        }
+
+        val result = deviceRepository.updateMyDeviceConsent(deviceConsent)
+
+        assertEquals(Result.Success(true), result)
+    }
+
+    @Test
+    fun `test update my device consent Error`() {
+        val errorMessage = "Error test"
+        `when`(deviceNetworkDataSource.updateMyDeviceConsent(deviceConsent,
+            Any() as (Exception?, Boolean?) -> Unit
+        )).thenAnswer {
+            val callback = it.arguments[1] as (Throwable?, Boolean?) -> Unit
+            callback(Throwable(errorMessage), null)
+        }
+
+        val result = deviceRepository.updateMyDeviceConsent(deviceConsent)
+
+        assertEquals(Result.Error(Throwable(errorMessage) as Exception), result)
+    }
+
 }
